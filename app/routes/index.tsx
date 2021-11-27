8dce8a933b19e2fa550c4ced10efa24553d8e7d8
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-27 17:39:58 -0500

    feat: 🎸 add coins-market fetch

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,49 +1,28 @@
 import type { MetaFunction, LoaderFunction } from 'remix'
 import { useLoaderData, json } from 'remix'
+import type { CoinMarket } from '~/module-types/coins-markets'
 
 type IndexData = {
-  resources: Array<{ name: string; url: string }>
-  demos: Array<{ name: string; to: string }>
+  coinsMarkets: CoinMarket[]
 }
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
 // https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-      {
-        name: 'Remix Docs',
-        url: 'https://remix.run/docs',
-      },
-      {
-        name: 'React Router Docs',
-        url: 'https://reactrouter.com/docs',
-      },
-      {
-        name: 'Remix Discord',
-        url: 'https://discord.gg/VBePs6d',
-      },
-    ],
-    demos: [
-      {
-        to: 'demos/actions',
-        name: 'Actions',
-      },
-      {
-        to: 'demos/about',
-        name: 'Nested Routes, CSS loading/unloading',
-      },
-      {
-        to: 'demos/params',
-        name: 'URL Params and Error Boundaries',
-      },
-    ],
-  }
+export let loader: LoaderFunction = async () => {
+  const response = await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false',
+  )
+
+  const responseData = await response.json()
+
+  let coinsMarkets: IndexData['coinsMarkets'] = responseData
 
   // https://remix.run/api/remix#json
-  return json(data)
+  return json({
+    coinsMarkets,
+  })
 }
 
 // https://remix.run/api/conventions#meta
@@ -56,9 +35,9 @@ export let meta: MetaFunction = () => {
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData<IndexData>()
+  let { coinsMarkets } = useLoaderData<IndexData>()
 
-  console.log({ data })
+  console.log(coinsMarkets)
 
   return (
     <div>
