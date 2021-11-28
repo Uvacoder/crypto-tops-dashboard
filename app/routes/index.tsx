ffa10524e33fe25b2c0784159c7f4816c5027e80
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-27 19:48:00 -0500

    feat: 🎸 add coins list table

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react'
+import { Column, useTable } from 'react-table'
 import type { MetaFunction, LoaderFunction } from 'remix'
 import { useLoaderData, json } from 'remix'
 import { MainLayout } from '~/layouts/main/main-layout'
@@ -29,8 +31,8 @@ export let loader: LoaderFunction = async () => {
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: 'Remix Starter',
-    description: 'Welcome to remix!',
+    title: 'Crypto Market',
+    description: 'Welcome to Crypto Market',
   }
 }
 
@@ -38,14 +40,102 @@ export let meta: MetaFunction = () => {
 export default function Index() {
   let { coinsMarkets } = useLoaderData<IndexData>()
 
-  coinsMarkets
+  const memoizedData = React.useMemo(() => coinsMarkets, [coinsMarkets.length])
+
+  const tableColumns = React.useMemo(
+    (): Column<CoinMarket>[] => [
+      {
+        Header: 'Name',
+        accessor: 'name',
+        Cell: ({ cell: { value }, row: { original } }) => {
+          return (
+            <span className="flex gap-1 items-center">
+              <img src={original.image} className="w-3" />
+              <span className="float-right text-secondary font-bold">{value}</span>
+            </span>
+          )
+        },
+      },
+      {
+        Header: 'Symbol',
+        accessor: 'symbol',
+      },
+      {
+        Header: 'Price',
+        accessor: 'current_price',
+        Cell: ({ cell: { value } }) => <span>$ {value}</span>,
+      },
+      {
+        Header: 'Change',
+        accessor: 'price_change_percentage_24h',
+        Cell: ({ cell: { value } }) => {
+          if (value > 0) {
+            return <span className="text-success">{value} %</span>
+          }
+
+          return <span className="text-error">{value} %</span>
+        },
+      },
+      {
+        Header: 'Market Cap',
+        accessor: 'market_cap',
+        Cell: ({ cell: { value } }) => <span>${value}</span>,
+      },
+      {
+        Header: 'Volume',
+        accessor: 'total_volume',
+      },
+      {
+        Header: 'Circulating Supply',
+        accessor: 'circulating_supply',
+        Cell: ({ cell: { value } }) => <span>${value}</span>,
+      },
+      {
+        Header: 'Last Updated',
+        accessor: 'last_updated',
+        Cell: ({ cell: { value } }) => <span>{value}</span>,
+      },
+    ],
+    [],
+  )
+
+  const tableInstance = useTable({ columns: tableColumns, data: memoizedData })
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
 
   return (
     <MainLayout>
       <div className="container my-3">
-        <div className="bg-primary">
-          <h1>Hello World</h1>
-        </div>
+        <table {...getTableProps()} className="shadow w-full">
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()} className="p-1.5 border-b">
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row)
+
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td {...cell.getCellProps()} className="p-1.5">
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     </MainLayout>
   )
